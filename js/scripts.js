d84b501a5ee153c67c6c69f63b68efc7772b7c89
William Kim <wkim@williams-mbp.home>
2020-05-20 17:43:06 -0700

    First push

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
-jQuery(function() {
+jQuery(document).ready(function($){
 
-	$('#my_button').click(function () {
-	});
+	$('#button').click(function(){
 
+		//set var 'src' to point to #image-box element and find attribute for first available src value
+		let src = $('#image-box).attr('src');
+		//if the current image is force.png, change it to dark.png
+		if (src == 'force.png')
+		$('#image-box').attr('src','/images/dark.png');
+		//if the current image is dark.png, change it to light.png
+	}else if(src == 'dark.png') {
+		$('#image-box').attr('src','/images/light.png');
+		//if the current image is light.png, change it to force.png
+	}else if(src == 'light.png') {
+		$('#image-box').attr('src','/images/force.png');
 });
